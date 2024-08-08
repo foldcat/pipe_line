cbd445d0848f9559dddc06655dc4d4c69d3b6fb1
foldcat <151979723+foldcat@users.noreply.github.com>
2024-08-08 11:56:38 +0800

    log relay update

--- lib/relay/core.ex
+++ lib/relay/core.ex
@@ -4,23 +4,27 @@ defmodule PipeLine.Relay.Core do
   to multiple Discord channels.
   """
 
+  require Logger
+  import IO.ANSI
+
   @doc """
   pipe_single_msg takes a message object and sends it 
   to every channel that is registered BUT the channel 
   id supplied to from_channel argument
   """
-  require Logger
-  import IO.ANSI
-
   @spec pipe_msg(Nostrum.Struct.Message, String.t()) :: :ok
   def pipe_msg(msg, from_channel) do
     cache_lookup = :ets.tab2list(:chan_cache)
 
     Enum.each(cache_lookup, fn {chanid} ->
-      Logger.info("""
-        relaying message #{blue() <> Integer.to_string(msg.id) <> reset()}
-        to channel #{blue() <> chanid <> reset()}
-      """)
+      # do not relay message back to the original channel
+      if not (Integer.to_string(msg.channel_id) == from_channel) do
+        Logger.info("""
+          relaying message #{blue() <> Integer.to_string(msg.id) <> reset()}
+          to channel #{blue() <> chanid <> reset()} 
+          from channel #{blue() <> from_channel <> reset()}
+        """)
+      end
 
       nil
     end)
@@ -30,7 +34,11 @@ defmodule PipeLine.Relay.Core do
 
   @spec relay_msg(Nostrum.Struct.Message) :: :ok
   def relay_msg(msg) do
-    cache_lookup = :ets.lookup(:chan_cache, Integer.to_string(msg.channel_id))
+    cache_lookup =
+      :ets.lookup(
+        :chan_cache,
+        Integer.to_string(msg.channel_id)
+      )
 
     # said message is inside registered channel list
     if not Enum.empty?(cache_lookup) do
@@ -39,7 +47,7 @@ defmodule PipeLine.Relay.Core do
       #{blue() <> Integer.to_string(msg.id) <> reset()}
       """)
 
-      pipe_msg(msg, msg.channel_id)
+      pipe_msg(msg, Integer.to_string(msg.channel_id))
     end
 
     :ok
