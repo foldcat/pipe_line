37bb2984b8e430e8c7bce593cb6eac93a8272326
foldcat <151979723+foldcat@users.noreply.github.com>
2024-08-30 14:59:57 +0800

    bump version

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule PipeLine.MixProject do
   def project do
     [
       app: :pipe_line,
-      version: "1.0.0",
+      version: "1.1.0",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps()
